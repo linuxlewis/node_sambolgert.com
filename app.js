899020cff697422bef41bf77fc4890c3b02c60a9
Sam Bolgert <sbolgert@gmail.com>
2012-01-16 17:56:53 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express.createServer(
+    express.logger()
+    );
+app.register('.html', require('ejs'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.configure(function(){
+    app.use(app.router);
+    app.use('/static', express.static(__dirname + '/static'));
+    app.use(express.errorHandler({
+        dumpExceptions: true, showStack:true }));
+    });
+
+app.get('/', function(req, res){
+        res.render('home.html', {title: 'Sam Bolgert Softwarepreneur' });
+    });
+
+app.get('/works', function(req, res){
+        res.render('works.html', {title: 'Sam Bolgert Softwarepreneur | Works' });
+    });
+
+app.get('/contact', function(req, res){
+        res.render('contact.html', {title: 'Sam Bolgert Softwarepreneur | Contact' });
+    });
+
+app.listen(3000);
+console.log("Server running...");
+
